3d68cd118f804d468b431bd4431907b14294ad24
Patrick Lourenz <patricklourenz@gmail.com>
2022-08-04 16:35:20 +1000

    yellow taxi download 2021-2022

--- scripts/download.py
+++ scripts/download.py
@@ -13,7 +13,7 @@ for target_dir in ('tlc_data', 'other_data'): # taxi_zones should already exist
     if not os.path.exists(output_relative_dir + target_dir):
         os.makedirs(output_relative_dir + target_dir)
         
-YEARS = ['2020', '2021', '2022']
+YEARS = ['2021', '2022']
 # adjust the range function to the numerical months i.e 1 = jan, 2 = feb, etc...
 # MONTHS = range(1, 13)
 MONTHS = range(1, 13)
@@ -24,10 +24,12 @@ URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_
 tlc_output_dir = output_relative_dir + 'tlc_data'
 
 for year in YEARS:
+    if year == "2022":
+        MONTHS = range(1, 5)
     for month in MONTHS:
         # 0-fill i.e 1 -> 01, 2 -> 02, etc
         month = str(month).zfill(2) 
-        print(f"Begin year {year} - month {month}")
+        print(f"Begin downloading Yellow Taxi Trip data year {year} - month {month}")
 
         # generate url
         url = f'{URL_TEMPLATE}{year}-{month}.parquet'
@@ -36,4 +38,4 @@ for year in YEARS:
         # download
         urlretrieve(url, output_dir) 
 
-        print(f"Completed month {month}")
+        print(f"Completed year {year} - month {month}")
