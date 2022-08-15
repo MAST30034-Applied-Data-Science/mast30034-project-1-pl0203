dacc1f3dd189e0551b1b40384b537953eb48dae4
Patrick Lourenz <patricklourenz@gmail.com>
2022-08-16 03:27:44 +1000

    no changes

--- scripts/download.py
+++ scripts/download.py
@@ -8,34 +8,35 @@ if not os.path.exists(output_relative_dir):
     os.makedirs(output_relative_dir)
     
 # create the paths for the data
-for target_dir in ('tlc_data', 'other_data'): # taxi_zones should already exist
+for target_dir in ('tlc_data', 'other_data'): # taxi_zones, weather should already exist
     if not os.path.exists(output_relative_dir + target_dir):
         os.makedirs(output_relative_dir + target_dir)
-        
-YEARS = ['2019']
+
+                    
+YEARS = ['2018', '2019']
 MONTHS = range(1, 13)
 
-# this is the URL template as of 07/2022
-URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_"
+# Taxi URL template as of 08/2022
+yellow_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_"#year-monnth
 
 # data output directory is `data/tlc_data/`
-tlc_output_dir = output_relative_dir + 'tlc_data'
-
+yellow_output_dir = output_relative_dir + 'tlc_data'
+weather_output_dir = output_relative_dir + 'other_data/weather'
+                    
 # download yellow taxi trip data
 for year in YEARS:
     for month in MONTHS:
         # 0-fill i.e 1 -> 01, 2 -> 02, etc
         month = str(month).zfill(2) 
-        print(f"Begin downloading Yellow Taxi Trip data year {year} - month {month}")
+        print(f"Begin downloading yellow taxi trip data year {year} - month {month}")
 
         # generate url
-        url = f'{URL_TEMPLATE}{year}-{month}.parquet'
+        url = f'{yellow_url}{year}-{month}.parquet'
         # generate output location and filename
-        output_dir = f"{tlc_output_dir}/{year}-{month}.parquet"
+        output_dir = f"{yellow_output_dir}/{year}-{month}.parquet"
         # download
         urlretrieve(url, output_dir) 
 
         print(f"Completed year {year} - month {month}")
-
-
+    
     
