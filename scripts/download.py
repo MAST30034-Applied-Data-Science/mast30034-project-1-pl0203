52604bec8a803d33e35cb2864ce6e8e81ca73b2d
Patrick Lourenz <patricklourenz@gmail.com>
2022-08-22 02:12:44 +1000

    added train weather data download

--- scripts/download.py
+++ scripts/download.py
@@ -8,7 +8,7 @@ if not os.path.exists(output_relative_dir):
     os.makedirs(output_relative_dir)
     
 # create the paths for the data
-for target_dir in ('tlc_data', 'test_data'): 
+for target_dir in ('tlc_data', 'test_data', 'weather_data'): 
     if not os.path.exists(output_relative_dir + target_dir):
         os.makedirs(output_relative_dir + target_dir)
 
@@ -37,10 +37,19 @@ for year in YEARS:
         urlretrieve(url, output_dir) 
 
         print(f"Completed year {year} - month {month}")
+        
+# download weather data
+weather_url = "https://www.ncei.noaa.gov/data/global-hourly/access/" #/year/74486094789.csv
+for year in YEARS:
+    print(f"Begin downloading {year} weather data")
+    output_dir = f"{output_relative_dir}/weather_data/{year}-weather.csv"
+    url = f'{weather_url}{year}/74486094789.csv'
+    urlretrieve(url, output_dir) 
+    print(f"Completed year {year} weather data")
 
 # Test data download
 # download taxi test data
-print(f"Begin downloading yellow taxi trip data year {year} - month {month}")
+print(f"Begin downloading yellow taxi trip data year 2020 - month 01")
 
 year = "2020"
 month = "01"
